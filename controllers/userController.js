4e6230894b4a561751c16b7bf960b6a56b69b8c6
Leon <leonissaclivi@gmail.com>
2025-05-07 19:04:21 +0530

    changes samesite to none

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,8 +48,8 @@ const userLogin = async (req, res, next) => {
         res
             .cookie("token", token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                sameSite: "Lax",
+                secure: true,
+                sameSite: 'None',
                 maxAge: 24 * 60 * 60 * 1000 // 1 day
             })
             .json({ success: true, message: "Login successful" });
